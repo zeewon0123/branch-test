7e6e110993df63ae483c2da23db37461869caadc
이지원 <ljw50210@gmail.com>
2024-12-07 03:36:53 +0900

    Update term-2.py
    modify dog jump height & fps

--- term-1.py
+++ term-1.py
@@ -1,12 +1,8 @@
-# python game with pygame : Jumping dino
-# by. BlockDMask
 import pygame
 import sys
 
-# step1 : set screen, fps
-
 pygame.init()
-pygame.display.set_caption('Jumping dog')
+pygame.display.set_caption('Jumping dino')
 MAX_WIDTH = 800
 MAX_HEIGHT = 400
 
@@ -16,6 +12,24 @@ def main():
     screen = pygame.display.set_mode((MAX_WIDTH, MAX_HEIGHT))
     fps = pygame.time.Clock()
 
+    # dino
+    imgdog1 = pygame.image.load('dogwalk1.png')
+    imgdog2 = pygame.image.load('dogwalk2.png')
+    dog_height = imgdog1.get_size()[1]
+    dog_bottom = MAX_HEIGHT - dog_height
+    dog_x = 50
+    dog_y = dog_bottom
+    jump_top = 400
+    leg_swap = True
+    is_bottom = True
+    is_go_up = False
+
+    # tree
+    imgTree = pygame.image.load('tree.png')
+    tree_height = imgTree.get_size()[1]
+    tree_x = MAX_WIDTH
+    tree_y = MAX_HEIGHT - tree_height
+
     while True:
         screen.fill((255, 255, 255))
 
@@ -24,12 +38,45 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if is_bottom:
+                    is_go_up = True
+                    is_bottom = False
+
+        # dino move
+        if is_go_up:
+            dog_y -= 10.0
+        elif not is_go_up and not is_bottom:
+            dog_y += 10.0
+
+        # dino top and bottom check
+        if is_go_up and dog_y <= jump_top:
+            is_go_up = False
+
+        if not is_bottom and dog_y >= dog_bottom:
+            is_bottom = True
+            dog_y = dog_bottom
+
+        # tree move
+        tree_x -= 12.0
+        if tree_x <= 0:
+            tree_x = MAX_WIDTH
 
         # draw tree
+        screen.blit(imgTree, (tree_x, tree_y))
+
+        # draw dino
+        if leg_swap:
+            screen.blit(imgdog1, (dog_x, dog_y))
+            leg_swap = False
+        else:
+            screen.blit(imgdog2, (dog_x, dog_y))
+            leg_swap = True
+
+        # update
         pygame.display.update()
-        fps.tick(30)
+        fps.tick(60)
 
 
 if __name__ == '__main__':
     main()
-#reportMissingImports [boolean or string, optional]: Generate or suppress diagnostics for imports that have no corresponding imported python file or type stub file. The default value for this setting is "error".
